4d538b8d074d6d336f999c8a0ab8844097cb53f1
aksonai <fatjongerra@gmail.com>
2020-02-18 09:26:37 +0100

    [UPD] refactored the solution of two-sum in Python

--- two-sum/solution.py
+++ two-sum/solution.py
@@ -1,16 +1,13 @@
 class Solution:
     def twoSum(self, nums, target):
-        the_dict = {}
-        for i, num_1 in enumerate(nums):
-            the_dict[target-num_1] = i
-        
-        for j, num_2 in enumerate(nums):
-            i = the_dict.get(num_2, None)
-            if i and i != j:
-                return [j,i]          
+        complement_dict = {}
+        for i, num in enumerate(nums):
+            if num in complement_dict:
+                return [complement_dict[num], i]
+            else:
+                complement_dict[target - num] = i
         return []
             
-
 
 sol = Solution()
 print(sol.twoSum([2, 7, 11, 15], 9))
