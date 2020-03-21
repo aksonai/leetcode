12a4606a1333798a4ef70e3e31e652a460597590
aksonai <fatjongerra@gmail.com>
2020-03-21 18:27:31 +0100

    [ADD] partial solution to the zigzag conversion

--- 0006-zigzag-conversion/solution.py
+++ 0006-zigzag-conversion/solution.py
@@ -1,14 +1,33 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         arr = []
-        i = 0
-        while i < len(s):
-            arr.append(s[i:i+numRows])
-            for j in range(numRows-2):
-                arr.append(s[i+numRows+j])
-            i += numRows
-           
+        ss = ''
+        for i, c in enumerate(s):
+            rem = i % (2 * numRows - 2)
+            if rem in range(numRows, 2 * numRows - 2):
+                arr.append(c)
+            elif rem == numRows - 1:
+                ss += c
+                arr.append(ss)
+                ss = ''
+            else:
+                ss += c
+                
+        if ss:
+            arr.append(ss)
+            
+            
         print(arr)
+            
+        
+        # i = 0
+        # while i < len(s):
+        #     arr.append(s[i:i+numRows])
+        #     for j in range(numRows-2):
+        #         arr.append(s[i+numRows+j])
+        #     i += 2 * numRows - 2
+           
+        # print(arr)
         
         
 sol = Solution()
