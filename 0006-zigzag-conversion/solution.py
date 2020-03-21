e410508a9c13d0f617f841612b5d6d637a557f2f
aksonai <fatjongerra@gmail.com>
2020-03-21 18:52:40 +0100

    [ADD] final solution to the zigzag conversion

--- 0006-zigzag-conversion/solution.py
+++ 0006-zigzag-conversion/solution.py
@@ -1,34 +1,20 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        arr = []
-        ss = ''
+        if len(s) < 2 or numRows == 1:
+            return s
+        
+        arr = [''] * numRows
+        
         for i, c in enumerate(s):
             rem = i % (2 * numRows - 2)
             if rem in range(numRows, 2 * numRows - 2):
-                arr.append(c)
-            elif rem == numRows - 1:
-                ss += c
-                arr.append(ss)
-                ss = ''
+                arr[2 * numRows - 2 - rem] += c
             else:
-                ss += c
+                arr[rem] += c
                 
-        if ss:
-            arr.append(ss)
-            
-            
-        print(arr)
-            
-        
-        # i = 0
-        # while i < len(s):
-        #     arr.append(s[i:i+numRows])
-        #     for j in range(numRows-2):
-        #         arr.append(s[i+numRows+j])
-        #     i += 2 * numRows - 2
-           
-        # print(arr)
-        
-        
+        return ''.join(arr)
+    
+    
+# Testing the solution  
 sol = Solution()
-sol.convert('PAYPALISHIRING', 4)
+print(sol.convert('PAYPALISHIRING', 4))
