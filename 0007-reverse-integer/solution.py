b0ee9f555006d03120f4423ee40d80f462ee3c78
aksonai <fatjongerra@gmail.com>
2020-03-24 19:51:57 +0100

    [FIX] solution to reverse integer in Python

--- 0007-reverse-integer/solution.py
+++ 0007-reverse-integer/solution.py
@@ -1,13 +1,24 @@
 class Solution:
+    def reverse_positive(self, x: int) -> int:
+        x = abs(x)
+        y = 0
+        while x:
+            quot, rem = divmod(x, 10)
+            x = quot
+            y = y * 10 + rem
+        return y
+        
     def reverse(self, x: int) -> int:
-        if x < - 2 ** 31 or x > 2 ** 31:
-            return 0
-        elif x < 0:
-            str_int = str(x)[1:]
-            return - int(str_int[::-1])
+        y = self.reverse_positive(x)
+        if - y >= - 2 ** 31 and x < 0:
+            return - y
+        elif y < 2 ** 31 and x >= 0:
+            return y
         else:
-            return int(str(x)[::-1])
+            return 0
         
         
 sol = Solution()
 print(sol.reverse(-123))
+print(sol.reverse(0))
+print(sol.reverse(-2147483648))
